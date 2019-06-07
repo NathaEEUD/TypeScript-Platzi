7425ecef185f70bf4ad40dd4bbf9ce0e821b09a0
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-06-06 19:45:59 -0500

    feat: union-types in typescript

---
+++ union-types.ts
@@ -0,0 +1,21 @@
+type SumaParameter = number | string;
+type SumaReturnType = number | string;
+
+function Suma(num1: SumaParameter, num2: SumaParameter): SumaReturnType {
+  return String(num1) + num2;
+}
+
+interface Interface1 {
+  prop1: number;
+}
+
+interface Interface2 {
+  prop2: number;
+}
+
+type InterfaceMix = Interface1 | Interface2;
+
+const interfaceMix: InterfaceMix = {
+  prop1: 1,
+  prop2: 2
+}
