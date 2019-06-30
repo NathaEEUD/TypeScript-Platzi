d32b825902fff854f4d749fe5a79cba9f1d97f25
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-06-30 10:56:31 -0500

    feat: implementation of the deletion of a player in the table

--- project-platzi/src/app/player-table/player-table.component.ts
+++ project-platzi/src/app/player-table/player-table.component.ts
@@ -1,7 +1,9 @@
-import { PlayerService, PlayersTableHeaders } from './../services/player.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Player } from '../interfaces/player';
+import { TeamService } from './../services/team.service';
+import { PlayerService, PlayersTableHeaders } from './../services/player.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-player-table',
@@ -14,7 +16,7 @@ export class PlayerTableComponent implements OnInit {
   public selectedPlayer: Player;
   public showModal = false;
 
-  constructor(private playerService: PlayerService) {}
+  constructor(private playerService: PlayerService, private teamService: TeamService) {}
 
   ngOnInit() {
     this.players$ = this.playerService.getPlayers();
@@ -34,6 +36,25 @@ export class PlayerTableComponent implements OnInit {
     setTimeout(() => {
       window.location.replace('#open-modal');
     }, 0);
+  }
+
+  deletePlayer(player: Player) {
+    console.log('deletePlayer player:::::', player);
+    this.teamService
+      .getTeams()
+      .pipe(take(1))
+      .subscribe(teams => {
+        console.log('deletePlayer teams:::::', teams);
+        const modifiedPlayers = teams[0].players ? teams[0].players.filter((p: any) => p.key !== player.$key) : teams[0].players;
+        console.log('deletePlayer modifiedPlayers:::::', modifiedPlayers);
+        const formattedTeam = {
+          ...teams[0],
+          players: [...modifiedPlayers]
+        };
+        console.log('deletePlayer formattedTeam:::::', formattedTeam);
+        this.playerService.deletePlayer(player.$key);
+        this.teamService.editTeam(formattedTeam);
+      });
   }
 
   closeDialog() {
